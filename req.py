3b985a29d5c8eeb180b83a115b7283db8344c1f8
s.v.zhiltsov <s.v.zhiltsov@newcontact.su>
2019-08-23 17:15:06 +0400

    modified

--- req.py
+++ req.py
@@ -1,7 +1,7 @@
 import requests
 
 
-url = 'https://coursehunter.net/course/asinhronnye-metody-i-primery-v-python'
+url = 'https://coursehunter.net/course/algoritmy-dlya-razrabotchikov-chast-1'
 headers = {
         'Host': 'coursehunter.net',
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0',
@@ -10,7 +10,7 @@ headers = {
         'Accept-Encoding': 'gzip, deflate, br',
         'Referer': 'https://coursehunter.net/sign-in',
         'Connection': 'keep-alive',
-        'Cookie': 'locale=en; _ga=GA1.2.8342895.1564666488; CHUNTERS=a63a2524117cd4608dce03f368aafd39; locale=en; redirect_after_login=https://coursehunter.net/course/asinhronnye-metody-i-primery-v-python; user_ident=e307d0bf-118d-4283-a5c0-c2fa3a222557; accessToken=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImp0aSI6IjRmMWcyM2ExMmFhIn0.eyJpc3MiOiJodHRwczpcL1wvY291cnNlaHVudGVyLm5ldCIsImF1ZCI6Imh0dHBzOlwvXC9jb3Vyc2VodW50ZXIubmV0IiwianRpIjoiNGYxZzIzYTEyYWEiLCJpYXQiOjE1NjY1NDUwNzcsIm5iZiI6MTU2NjU0NTEzNywiZXhwIjoxNTY3MTQ5ODc3LCJ1c2VyX2lkIjoiMzA4MDQiLCJlX21haWwiOiJ1Z3Rlcm5AZ21haWwuY29tIn0.NNNev1sJ9-8EaxdA69aXAOJxtm59GfefjMQqWgxF38Y; _gid=GA1.2.1659369943.1566370458; ch_quiz=0623c1b59a6fd0d431bb09a0a363bca3; _gat=1',
+        'Cookie': 'locale=en; _ga=GA1.2.8342895.1564666488; CHUNTERS=a63a2524117cd4608dce03f368aafd39; locale=en; redirect_after_login=https://coursehunter.net/course/algoritmy-dlya-razrabotchikov-chast-1; user_ident=652cb667-8b10-48ae-bf83-d9e1131d6973; accessToken=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImp0aSI6IjRmMWcyM2ExMmFhIn0.eyJpc3MiOiJodHRwczpcL1wvY291cnNlaHVudGVyLm5ldCIsImF1ZCI6Imh0dHBzOlwvXC9jb3Vyc2VodW50ZXIubmV0IiwianRpIjoiNGYxZzIzYTEyYWEiLCJpYXQiOjE1NjY1NjQzMDYsIm5iZiI6MTU2NjU2NDM2NiwiZXhwIjoxNTY3MTY5MTA2LCJ1c2VyX2lkIjoiMzA4MDQiLCJlX21haWwiOiJ1Z3Rlcm5AZ21haWwuY29tIn0.ATou1NVLHeSS1ts5TKqeVBwkt7wsumd5XXyqlcWAMtE; _gid=GA1.2.1659369943.1566370458; ch_quiz=0623c1b59a6fd0d431bb09a0a363bca3; _gat=1',
         'Upgrade-Insecure-Requests': '1',
         'Cache-Control': 'max-age=0',
     }
--- req2.py
+++ req2.py
@@ -1,4 +1,23 @@
 import urllib.request
+import requests
+from clint.textui import progress
+
 with open('./links_list.txt') as file:
     for url in file:
-        urllib.request.urlretrieve(url, './course/{}'.format(url.split('/')[4].replace('\n', '')))
+
+        r = requests.get(url.replace('\n', ''), stream=True)
+
+        # urllib.request.urlretrieve(url, './course2/{}'.format(url.split('/')[4].replace('\n', '')))
+        # print('complete {}'.format(url))
+
+        path = './{}'.format(url.split('/')[4].replace('\n', ''))
+
+        print(path)
+
+        with open(path, 'wb') as f:
+            total_length = int(r.headers.get('content-length'))
+            for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length / 1024) + 1):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+
